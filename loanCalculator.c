6534d116962ad91dba3ba4dde51496f881c990ed
cepstrum <stevesaidkhanian@gmail.com>
2018-09-12 21:29:13 -0700

    added loanCalculator assignment and updated askUserForInt.c/h to more generic myUserInput. Added askUserForFloat to myUserInput.h

---
+++ loanCalculator.c
@@ -0,0 +1,81 @@
+//You will ask the user to enter the loan amount, the number of years to pay back, and the yearly interest rate.
+
+
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include "myUserInput.h"
+
+#define PAYMENTSPERYEAR 12
+
+
+void getUserInput(float *loanAmount, int *yearsOfLoan, float *interestRate);
+float calculateMonthlyPayment(float loanAmount, int yearsOfLoan, float interestRate);
+float calculateTotalPayment(float, float);
+
+int main(void)
+{
+	float loanAmount;
+	int yearsOfLoan = 0;
+	float interestRate;
+	float monthlyPayment;
+
+	getUserInput(&loanAmount, &yearsOfLoan, &interestRate);
+	monthlyPayment = calculateMonthlyPayment(loanAmount, yearsOfLoan, interestRate);
+
+	printf("Your monthly payment is $%.2f\nYour total payment will be $%.2f", monthlyPayment, calculateTotalPayment(monthlyPayment, yearsOfLoan));
+}
+
+
+void getUserInput(float *loanAmount, int *yearsOfLoan, float *interestRate)
+{
+	
+	printf("Enter the amount of years for the loan " );	
+	while ((*yearsOfLoan = askUserForInt()) == 0)
+	{
+		printf("You entered 0 which is an invalid amount of years, enter at least 1 year ");
+	}
+
+	printf("You entered: %d\n", *yearsOfLoan);
+
+
+	printf("Enter the loan amount ");
+	*loanAmount = askUserForFloat();
+	printf("You entered $%f\n ", *loanAmount);
+	
+
+	printf("Enter the interest rate ");
+	*interestRate = askUserForFloat();
+	printf("You entered %f% \n", *interestRate);
+	
+}
+
+float calculateMonthlyPayment(float loanAmount, int yearsOfLoan, float interestRate)
+{
+	if (interestRate != 0)
+	{
+		float i, discount, onePlusI, num, denom, n;
+
+		i = (interestRate/100) / 12;
+		n = yearsOfLoan * 12;
+		
+		onePlusI = pow((1+i), n);
+
+		num = onePlusI - 1;
+		denom = i * onePlusI;
+
+		discount = num/denom;
+	
+
+		return loanAmount/discount;
+	}
+	else
+		return loanAmount/(yearsOfLoan * 12); // no interest rate means monthly payment
+
+}
+
+float calculateTotalPayment(float monthlyPayment, float numberOfYears)
+{
+	return monthlyPayment * numberOfYears * 12;
+}
---
+++ myUserInput.c
@@ -0,0 +1,121 @@
+#include "myUserInput.h"
+
+
+// asks user for input from STDIN and only allows user to proceed if they enter a number
+// returns a an integer representation of the number entered
+// decimal points are not allowed in input
+
+
+int askUserForInt()
+{
+	char inputArray[INT_MAX_LINE];
+	char *end;
+	int x;
+	short int flag;
+	int userInputAsInt;
+
+	for (x = 0; x < sizeof inputArray; x++) // initialize array to all 0's so that we can check total input and remove garbage
+	{
+		inputArray[x] = 0;
+	}
+
+	flag = 0;
+
+	while (flag == 0)
+	{
+		printf("using an integer value: "); // before calling this function printf something more specific like "Enter loan amount" (using integer value)
+		fgets(inputArray, sizeof inputArray, stdin);
+
+		for (x = 0; x < sizeof inputArray; x++)
+		{
+			if (((inputArray[x] > 47) && (inputArray[x] < 58)) || (inputArray[x] == 0)) //between 47 and 58 are numbers, 0 is null created earlier
+			{	
+				flag = 1;
+				//printf("I got a number! \n");
+			} 
+			else if (inputArray[x] == 10) //newline from fgets is replaced by 0
+			{
+				inputArray[x] = 0;
+				x--;
+			}
+			else
+			{ 
+
+				printf("I did not get a number, try again by entering only a positive whole number without characters, spaces, or other syntax \n");
+				flag = 0;
+				for (x = 0; x < sizeof inputArray; x++)
+				{
+					inputArray[x] = 0;
+				}
+				break;
+			}			
+		}	
+	}	
+
+	userInputAsInt = strtol(inputArray, &end, 10);
+	if (userInputAsInt == 2147483647 )
+		printf("The number you entered is equal to or exceeds the maximum allowable value for this program, we will use the maximum value of 2147483647 as your input\n");
+	return userInputAsInt;
+
+}
+
+
+
+
+float askUserForFloat()
+{
+	char inputArray[FLOAT_MAX_LINE];
+	char *end;
+	int x;
+	short int flag;
+	float userInputAsFloat;
+
+
+	for (x = 0; x < sizeof inputArray; x++) // initialize array to all 0's so that we can check total input and remove garbage
+	{
+		inputArray[x] = 0;
+	}
+
+	flag = 0;
+
+	while (flag == 0)
+	{
+		printf("using a float value: "); // before calling this function printf something more specific like "Enter loan amount" (using float value)
+		fgets(inputArray, sizeof inputArray, stdin);
+
+		for (x = 0; x < sizeof inputArray; x++)
+		{
+			if (((inputArray[x] > 47) && (inputArray[x] < 58)) || (inputArray[x] == 0) || (inputArray[x] == 46)) //between 47 and 58 are numbers in ascii, 0 is null created earlier, 46 is a decimal point
+			{	
+				flag = 1;
+				//printf("I got a number! \n");
+			} 
+			else if (inputArray[x] == 10) //newline from fgets is replaced by 0
+			{
+				inputArray[x] = 0;
+				x--;
+			}
+			else
+			{ 
+
+				printf("I did not get a number, try again by entering only a positive number without characters (with the exception of a decimal), spaces, or other syntax \n");
+				flag = 0;
+				for (x = 0; x < sizeof inputArray; x++)
+				{
+					inputArray[x] = 0;
+				}
+				break;
+			}			
+		}	
+	}	
+
+	userInputAsFloat = strtof(inputArray, &end);
+	if (userInputAsFloat >= 999999986991104)
+	{						
+		printf("You entered a number which was too high, we are using $999,999,986,991,104.0 instead");
+		userInputAsFloat = 999999986991104;
+	}
+		
+	return userInputAsFloat;
+
+}
