e1a3dcb8843f410d21a6ae8904d5ee090e5d973a
cepstrum <stevesaidkhanian@gmail.com>
2018-09-12 17:36:26 -0700

    changed to a header file for future use

---
+++ askUserForInt.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_LINE 15
+
+#ifndef ASKUSERFORINT_H_INCLUDED
+#define ASKUSERFORINT_H_INCLUDED
+
+int askUserForInt(void); // asks user for input from STDIN and only allows user to proceed if they enter a number
+
+#endif
